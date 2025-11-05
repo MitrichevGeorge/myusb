b4ba8f961b3f51228168d35760f92fe9e0427419
MitrichevGeorge <131480793+MitrichevGeorge@users.noreply.github.com>
2025-11-05 15:57:47 +0300

    Update i.ps1

--- i.ps1
+++ i.ps1
@@ -1,5 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show('Скрипт запущен!', 'Info', 'OK', 'Information')
+[System.Windows.Forms.MessageBox]::Show('RUNNNED', 'Info', 'OK', 'Information')
 
 
 function Download($from, $to) {
